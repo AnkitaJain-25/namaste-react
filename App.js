949476e7aca9e61f853cb2cd2b1aef4f3f98ceeb
Ankita Jain <ankita.jain25@outlook.com>
2023-09-16 19:38:06 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
 /**
  *
  * <div id="parent">
@@ -16,20 +19,23 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"), //for sibling create array of children
+    React.createElement("h1", {}, "This is Namaste React"),
+    React.createElement("h2", {}, "by Ankita Jain"), //for sibling create array of children
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"), //for sibling create array of children
   ]),
 ]);
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello World from React!"
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent); //object
+
+const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMP = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of files
+- Bundling
+- Compress files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
