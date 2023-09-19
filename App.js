f4202c12215ed1d6efe359facf5ecb93742c3e5d
Ankita Jain <ankita.jain25@outlook.com>
2023-09-19 14:50:09 +0530

    Episode-03: Laying the Foundation

--- App.js
+++ App.js
@@ -1,41 +1,22 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-/**
- *
- * <div id="parent">
- *  <div id="child">
- *      <h1>I'm an h1 tag</h1>
- *      <h2>I'm an h1 tag</h2>
- *  </div>
- *  <div id="child2">
- *      <h1>I'm an h1 tag</h1>
- *      <h2>I'm an h1 tag</h2>
- *  </div>
- * </div>
- *
- * ReactElement(Object) ==> HTML(Browser Understands)
- */
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React"),
-    React.createElement("h2", {}, "by Ankita Jain"), //for sibling create array of children
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"), //for sibling create array of children
-  ]),
-]);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
 );
 
-console.log(parent); //object
+// Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    {Title()}
+    <Title></Title>
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
